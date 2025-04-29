bd61f68fe32e17b1adc12fa9851522a839dc37a1
Ahmed Abdullah Ahmed <ahmeddev452@gmail.com>
2025-04-29 09:53:02 +0300

    AuthPage UI almost done

--- src/components/AuthComponents/AuthPage.jsx
+++ src/components/AuthComponents/AuthPage.jsx
@@ -1,34 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AuthPage.css'
 import { FaUser } from "react-icons/fa6";
 import { IoMdLock } from "react-icons/io";
 
 const AuthPage = () => {
+    const [isLogin,setIsLogin] = useState(true);
+    const [errorMessage,setErrorMessage] = useState(null);
+    
   return (
     <div className='wrapper'>
         <form action="">
-            <h1>Login</h1>
+            <h1>{(isLogin) ? 'Login':'Sign Up'}</h1>
+            {errorMessage && 
+            <div className='error-message'>
+                <p>{errorMessage}</p>
+            </div>}
             <div className='input-box'>
-                <input className='input' placeholder='username' type='text'/>
+                <input className='input' placeholder='username' type='text' required/>
                 <FaUser className='icon'/>
             </div>
             <div className='input-box'>
-                <input className='input' placeholder='password' type='password'/>
+                <input className='input' placeholder='password' type='password' required/>
                 <IoMdLock className='icon'/>
             </div>
+            
+            {!isLogin && 
             <div className='input-box'>
-                <input className='input' placeholder='confirm password' type='password'/>
+                <input className='input' placeholder='confirm password' type='password' required/>
                 <IoMdLock className='icon'/>
-            </div>
+            </div>}
 
+            {(isLogin) && 
             <div className='remember'>
                     <label><input type='checkbox'/>remember me</label>
-            </div>
+            </div>}
             <div className='sign-Button'>
-                <button type="submit">Sign In</button>
+                <button type="submit">{(isLogin) ? 'Login':'Sign Up'}</button>
             </div>
             <div className='signUp-link'>
-                <p>Don't have an Account? <a href='#'>SignUp</a></p>
+                <p>{isLogin ? 'Don\'t have an Account?':'already have an account?'} <a href='#' onClick={() => setIsLogin(!isLogin)}>{(isLogin)? 'SignUp':'Sign In'}</a></p>
             </div>
         </form>
     </div>
